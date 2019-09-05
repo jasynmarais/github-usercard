3c0046fc927249b3ba6c001eef734e96533cb5c8
Jasyn Marais <jasynmarais@gmail.com>
2019-09-05 14:35:41 +0200

    step 1

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,15 @@
+const cardsContainer = document.querySelector('.cards');
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/jasynmarais')
+	.then(response => {
+		console.log(response);
+		cardsContainer.appendChild(createCardComponent(response.data));
+	})
+  .catch(error => console.error(error));
+  
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
